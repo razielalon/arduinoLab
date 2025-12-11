e82ba1f147a4a73248d7243b6aae74b683970d9b
razielalon <razielalon@gmail.com>
2025-12-11 17:19:01 +0200

    changed address to 1 on gbn rx and added comments

--- roy_omri_code/lab4.rx_gbn/lab4.rx_gbn.ino
+++ roy_omri_code/lab4.rx_gbn/lab4.rx_gbn.ino
@@ -12,8 +12,8 @@ const uint8_t DATA_LEN = sizeof(Data) - 1;
 #define FRAME_SIZE   (HEADER_SIZE + DATA_LEN + CRC_SIZE)
 
 uint8_t frame_rx[FRAME_SIZE];
-int expected_frame = 0;
-static int N_counter = 0;      
+int expected_frame = 0; // keeps the SN of the next expected frame
+static int N_counter = 0; // how many frames recived
 
 float total_frames = 0, bad_frames = 0, succ_frame = 0; 
 float eff = 0.0;
@@ -37,7 +37,7 @@ void RX_func() {
     static float Error_prob = 0;
     int flag_receive;
 
-    flag_receive = readPackage(frame_rx, FRAME_SIZE);
+    flag_receive = readPackage(frame_rx, FRAME_SIZE); // 1 if got a package
   
     if (flag_receive == 1) {
 
@@ -46,6 +46,7 @@ void RX_func() {
         }
         N_counter++;
 
+        // get fields from the frame
         uint8_t destination_address = frame_rx[0];
         uint8_t source_address      = frame_rx[1];
         uint8_t frame_type          = frame_rx[2];
@@ -56,6 +57,7 @@ void RX_func() {
             length = DATA_LEN;
         }
 
+        // get the payload out of the frame
         uint8_t data[DATA_LEN] = {0};
         for (uint8_t i = 0; i < length; ++i) {
             data[i] = frame_rx[6 + i];
@@ -63,27 +65,29 @@ void RX_func() {
 
         int crc_index = HEADER_SIZE + length;  // 6 + DATA_LEN
 
+        // get the crc field from the frame
         unsigned long received_crc = 0;
         received_crc |= (unsigned long)frame_rx[crc_index + 0] << 24;
         received_crc |= (unsigned long)frame_rx[crc_index + 1] << 16;
         received_crc |= (unsigned long)frame_rx[crc_index + 2] << 8;
         received_crc |= (unsigned long)frame_rx[crc_index + 3];
 
+        // calculate crc of the frame
         unsigned long calculated_crc = calculateCRC(frame_rx, crc_index);
 
-        // check bad crc
+        // check for bad crc
         if (calculated_crc != received_crc) {
             Serial.println("CRC mismatch");
             Serial.println();  
             bad_frames++;
 
-            // ack on the latest frame
-            ack_frame[0] = 0x09;
-            ack_frame[1] = 0x19;
-            ack_frame[2] = 0;
-            ack_frame[3] = 0;
-            ack_frame[4] = 0;
-            ack_frame[5] = expected_frame;  // next expected
+            // build ack packet of latest frame
+            ack_frame[0] = 0x01; // Destination Address
+            ack_frame[1] = 0x11; // Source Address
+            ack_frame[2] = 0; // type = ACK
+            ack_frame[3] = 0; //  length = {0 for ACK}
+            ack_frame[4] = 0; // ACK/DATA = {0 for ACK}
+            ack_frame[5] = expected_frame;  // Next Expected SN (stays the same as it was before frame arrives)
             ack_frame[6] = (calculated_crc >> 24) & 0xFF;
             ack_frame[7] = (calculated_crc >> 16) & 0xFF;
             ack_frame[8] = (calculated_crc >> 8)  & 0xFF;
@@ -91,7 +95,7 @@ void RX_func() {
         }
 
         // frame is okay
-        else if (received_sn == expected_frame) {
+        else if (received_sn == expected_frame) { // if got the frame SN that was expected
             succ_frame++;
 
             Serial.print("Payload (SN ");
@@ -107,19 +111,19 @@ void RX_func() {
             expected_frame = (expected_frame + 1) % N;
 
             // ack building
-            ack_frame[0] = 0x09;
-            ack_frame[1] = 0x19;
-            ack_frame[2] = 0;
-            ack_frame[3] = 0;
-            ack_frame[4] = 0;
-            ack_frame[5] = expected_frame;
+            ack_frame[0] = 0x01; // Destination Address
+            ack_frame[1] = 0x11; // Source Address
+            ack_frame[2] = 0; // type = ACK
+            ack_frame[3] = 0; // length = {0 for ACK}
+            ack_frame[4] = 0; // ACK/DATA = {0 for ACK}
+            ack_frame[5] = expected_frame; // Next Expected SN - after raising by 1 with modulu
             ack_frame[6] = (calculated_crc >> 24) & 0xFF;
             ack_frame[7] = (calculated_crc >> 16) & 0xFF;
             ack_frame[8] = (calculated_crc >> 8)  & 0xFF;
             ack_frame[9] =  calculated_crc        & 0xFF;
         }
 
-        // out of order then
+        // if out of order (got SN it wasnt expected)
         else {
             bad_frames++;
             Serial.print("Out of order frame received. Expected SN: ");
@@ -128,12 +132,12 @@ void RX_func() {
             Serial.println(received_sn);
 
             // ack on the last good frame
-            ack_frame[0] = 0x09;
-            ack_frame[1] = 0x19;
-            ack_frame[2] = 0;
-            ack_frame[3] = 0;
-            ack_frame[4] = 0;
-            ack_frame[5] = expected_frame;
+            ack_frame[0] = 0x01; // Destination Address
+            ack_frame[1] = 0x11; // Source Address
+            ack_frame[2] = 0;    // type = ACK
+            ack_frame[3] = 0;    // length = {0 for ACK}
+            ack_frame[4] = 0;    // ACK/DATA = {0 for ACK}
+            ack_frame[5] = expected_frame; // Next Expected SN (without raising ++)
             ack_frame[6] = (calculated_crc >> 24) & 0xFF;
             ack_frame[7] = (calculated_crc >> 16) & 0xFF;
             ack_frame[8] = (calculated_crc >> 8)  & 0xFF;
